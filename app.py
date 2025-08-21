821a40e23a3d95cd03fc1f0f98f7a6c0b611bbfa
Shahrukh <shahcsevira@gmail.com>
2025-08-21 12:30:56 +0530

    developerA

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ def addition(a,b):
 def subtraction(a,b):    return a - b
 def multiplication(a,b):
     return a * b   
+def devide(a,b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
