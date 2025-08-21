dad5e0216b4e1078a6fb4127296ac4c0c8a14e96
Shahrukh <shahcsevira@gmail.com>
2025-08-21 12:42:46 +0530

    Substartction

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ def addition(a,b):
     return a + b
 
 
-def subtraction(a,b):    return a - b
+def subtraction(a,b,c):   
+    return a - b - c
+
 def multiplication(a,b):
     return a * b   
+
 def devide(a,b):
     if b == 0:
         return "Error: Division by zero"
